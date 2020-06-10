8c86c837ee9a60f5b5b1872227743f0e7363049f
mqqgithub <maqq1234>
2020-06-10 23:26:05 +0800

    1

--- classes/urls.py
+++ classes/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # django2.x 之后用正则表达式，必须引用re_path函数
     # re_path(r'article/(?P<article_id>[0-9]+), views.article_page, name='article_page'),
     # re_path(r'^index$', views.index),  # 访问地址中不要在加上‘/’,会自动加上
-    re_path(r'^index$', views.index, name='index'),  # 伪静态
+    re_path(r'^index/(?P<name>\w*)', views.index, name='index'),  # 伪静态
     re_path(r'^login$', views.login, name='login'),
     re_path(r'^user$', views.user, name='user'),
     re_path(r'^del_user/', views.del_user, name='del_user'),
